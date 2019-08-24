0f8090ed16d86140258ba842fa864bc408401ce0
JCTNEU <josepc.c.thull@gmail.com>
2019-08-24 14:11:48 -0600

    Working Mongodb connection in Application with Admin User and Password

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ var multer = require('multer');
 var upload = multer();
 const port = 3000;
 const app = express();
+const Schema = mongoose.Schema;
+
+mongoose.connect('mongodb+srv://Admin:1234@webfinal-k4s0e.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }).
+  catch(error => console.log(error));
+
+var UserSchema = new Schema({
+  Username: String,
+  Password: String,
+  IsManager: Boolean,
+  IsActive: Boolean,
+  Email: String,
+  QuestionAnswerId: Number
+}, { collection: 'Users' });
+
+var User = mongoose.model('User', UserSchema);
+
+var QuestionAnswerSchema = new Schema({
+Question1: Number,
+Question2: Number,
+Question3: Number,
+AnswerId : Number
+}, { collection: 'QuestionAnswers' });
+
+var Question = mongoose.model('Question', QuestionAnswerSchema);
+
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + "/public"));
@@ -14,6 +40,10 @@ app.listen(port, function(){
 });
 
 app.get('/', function(rec, res){
+    res.render("index");
+});
+
+app.post('/', function(rec, res){
     res.render("index");
 });
 
